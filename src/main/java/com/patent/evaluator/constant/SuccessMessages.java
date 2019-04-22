ed4e167af8214e0813984c91b4a09f0bbe27123d
BaturSeker <baturseker06@gmail.com>
2019-04-22 14:10:42 +0300

    adding new PasswordController class

--- src/main/java/com/patent/evaluator/constant/SuccessMessages.java
+++ src/main/java/com/patent/evaluator/constant/SuccessMessages.java
@@ -1,11 +1,15 @@
 package com.patent.evaluator.constant;
 
 public final class SuccessMessages {
+
+    // # USER
     public static final String USER_TEMP_PASSWORD_TITLE = "Kullanıcı şifre sıfırlama işlemi.";
 
+    // # ROLE
     public static final String ROLE_ASSIGN_TITLE = "Kullanıcıya yetki atama.";
     public static final String ROLE_ASSIGN_MESSAGE = "Kullanıcıya yetki atama işlemi başarılı bir şekilde gerçekleştirildi.";
 
+    //  # AUTHORITY
     public static final String AUTHORITY_UPDATE_TITLE = "Yetki güncelleme.";
     public static final String AUTHORITY_UPDATE_MESSAGE = "Yetki başarılı bir şekilde güncellendi.";
 
@@ -14,4 +18,14 @@ public final class SuccessMessages {
 
     public static final String AUTHORITY_ASSIGN_TITLE = "Yetkiye rol atama işlemi.";
     public static final String AUTHORITY_ASSIGN_MESSAGE = "Yetki rolleri başarılı bir şekilde atandı.";
+
+    //  # PASSWORD
+    public static final String VALIDATE_PASSWORD_TITLE = "Şifre Validasyonu";
+    public static final String VALIDATE_PASSWORD_MESSAGE = "Şifreniz uygulanan patterne uygun.";
+
+    public static final String NEW_PASSWORD_TITLE = "Şifre Güncelleme";
+    public static final String NEW_PASSWORD_MESSAGE = "Şifre başarılı bir şekilde güncellenmiştir.";
+
+    public static final String RESET_PASSWORD_TITLE = "Şifre Sıfırlama";
+    public static final String RESET_PASSWORD_MESSAGE = "Şifre başarılı bir şekilde sıfırlanmıştır.";
 }
---
+++ src/main/java/com/patent/evaluator/controller/PasswordController.java
@@ -0,0 +1,65 @@
+package com.patent.evaluator.controller;
+
+
+import com.patent.evaluator.constant.SuccessMessages;
+import com.patent.evaluator.dto.SetNewPasswordRequest;
+import com.patent.evaluator.dto.SuccessResponseDto;
+import com.patent.evaluator.dto.UserRequest;
+import com.patent.evaluator.service.api.password.PasswordRules;
+import com.patent.evaluator.util.PasswordGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.NoSuchAlgorithmException;
+
+@RestController
+@RequestMapping("/rest/password/")
+public class PasswordController {
+
+    private PasswordRules passwordRules;
+
+    @GetMapping(value = "getPassword")
+    @PreAuthorize("@CheckPermission.hasPermission(authentication)")
+    public ResponseEntity getPassword() {
+        PasswordGenerator passwordGenerator = new PasswordGenerator();
+        String password = null;
+        password = passwordGenerator.generate(10);
+        while (!passwordRules.isPasswordValid(password)) {
+            password = passwordGenerator.generate(10);
+        }
+        return new ResponseEntity<>(password, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "validatePassword")
+    @PreAuthorize("@CheckPermission.hasPermission(authentication)")
+    public ResponseEntity validatePassword(@Valid @RequestBody UserRequest userRequest) {
+        passwordRules.passwordValidation(userRequest);
+        return new ResponseEntity<>(new SuccessResponseDto(SuccessMessages.VALIDATE_PASSWORD_TITLE, SuccessMessages.VALIDATE_PASSWORD_MESSAGE), HttpStatus.OK);
+
+    }
+
+    @PutMapping(value = "setNewPassword/{userId}")
+    @PreAuthorize("@CheckPermission.hasPermission(authentication)")
+    public ResponseEntity setNewPassword(@PathVariable Long userId, @Valid @RequestBody SetNewPasswordRequest setNewPasswordRequest) throws NoSuchAlgorithmException {
+        setNewPasswordRequest.setUserId(userId);
+        passwordRules.setNewPassword(setNewPasswordRequest);
+        return new ResponseEntity<>(new SuccessResponseDto(SuccessMessages.NEW_PASSWORD_TITLE, SuccessMessages.NEW_PASSWORD_MESSAGE), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "resetPassword")
+    @PreAuthorize("@CheckPermission.hasPermission(authentication)")
+    public ResponseEntity resetPassword(@RequestParam("userId") Long userId) throws NoSuchAlgorithmException {
+        passwordRules.resetPassword(userId);
+        return new ResponseEntity<>(new SuccessResponseDto(SuccessMessages.RESET_PASSWORD_TITLE, SuccessMessages.RESET_PASSWORD_MESSAGE), HttpStatus.OK);
+    }
+
+    @Autowired
+    public void setPasswordRules(PasswordRules passwordRules) {
+        this.passwordRules = passwordRules;
+    }
+}
+
